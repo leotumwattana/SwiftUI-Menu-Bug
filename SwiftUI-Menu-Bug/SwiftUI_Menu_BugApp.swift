95f85175eb17e8f452bd600855e1b9efaf92663a
Leo Tumwattana <leo.tumwattana@gmail.com>
2023-06-16 15:40:16 +0800

    Initial Commit

---
+++ SwiftUI-Menu-Bug/SwiftUI_Menu_BugApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Menu_BugApp.swift
+//  SwiftUI-Menu-Bug
+//
+//  Created by Leo Tumwattana on 16/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Menu_BugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
